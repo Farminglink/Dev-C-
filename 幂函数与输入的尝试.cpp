917785fd435cff2d56b27c76549114cc31bf2bcd
Farminglink <Q638.119@outlook.com>
2025-09-26 18:49:41 +0800

    Add files via upload

---
+++ 幂函数与输入的尝试.cpp
@@ -0,0 +1,30 @@
+/*
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a;
+	printf("请输入一个整数：");
+	scanf("%d",&a);
+	long long b = (long long)pow(a,5);
+	printf("%d的五次方是：%d",a,b);
+	
+	return 0;
+	
+}
+*/
+#include <stdio.h>
+
+int main()
+{
+	int i,j;
+	i = 255;
+	j = 255;
+	
+	printf("i 的值是 %d\n",(signed char)i);
+	printf("j 的值是 %d\n",(unsigned char)j);
+	
+	return 0;
+	
+}
