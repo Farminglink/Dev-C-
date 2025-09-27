fdb430b346ba13cf1c2520a7f92c98107ed2fe00
Farminglink <Q638.119@outlook.com>
2025-09-27 09:55:10 +0800

    Add files via upload

---
+++ S1E8.cpp
@@ -0,0 +1,37 @@
+/*
+#include <stdio.h>
+
+int main()
+{
+	float H,S;
+	printf("请输入华氏度：");
+	scanf("输入华氏度：%f",&H);
+	S = (H-32)*5/9;
+	printf("转换为摄氏度是：%.2f",S);
+	
+	return 0;
+	
+}
+*/
+/*
+#include <stdio.h>
+
+float H,W;
+char N[100];
+
+int main()
+{
+	printf("欢迎使用本转换器，我将为您提供由cm转换为in，由kg换成lb:");
+	printf("请输入你的姓名：");
+	scanf("%99s",N);
+	printf("请输入你的身高（cm）：");
+	scanf("%f",&H);
+	printf("请输入你的体重（kg）：");
+	scanf("%f",&W);
+	H = H/2.54;
+	W = W/0.45359;
+	printf("%s的体重是%.2f英镑，身高是%.2f英寸。",N,W,H);
+	return 0;
+	
+}
+*/
